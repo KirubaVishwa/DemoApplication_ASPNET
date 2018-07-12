6157c72f860701405c0908d1da4046f9627a0413
kvenkatesa22 <kirubanandam.venkatesan@dxc.com>
2018-07-12 12:21:33 +0530

    Added Demo application

---
+++ DemoApplication/Tutorial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoApplication
+{
+    public class Tutorial
+    {
+        public String Name;
+        public Tutorial()
+        {
+            Name = "Guru99 - ASP.Net";
+        } 
+    }
+}
